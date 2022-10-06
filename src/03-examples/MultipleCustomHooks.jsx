e24c130f3f0f46c615ba4541199d104f6a695764
Rafael <f.rafael.temich24@gmail.com>
2022-10-06 14:22:14 -0500

    useRef

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import { useFetch } from '../hooks/useFetch'
-
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote } from './'
 export const MultipleCustomHooks = () => {
-    const { data, isLoading, hasError } = useFetch('https://www.breakingbadapi.com/api/quotes/1');
-    console.log({ data, isLoading, hasError });
+  const { increment, counter } = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    // console.log({ data, isLoading, hasError });
+    // if variable data contains no data yields undefinied and is equivalent to false, that's why the second condition doesn't take place
+    // but if variable data contains data yields an array and is equivalent to true, so the second condition take place and return data in position 0
+    // const { author, quote } = !!data && data.length && data[0];
+    
+    const quoteData = !!data && data.length && data[0];
   return (
     <>
         <h1>BreakingBad Quotes</h1>
         <hr />
 
+        {
+          (isLoading)
+                ? 
+                  <LoadingQuote />
+                : 
+                  <Quote { ...quoteData } />
+        }
 
+        <button
+            className='btn btn-primary'
+            onClick={ () => increment() }
+            disabled={isLoading}
+            >
+          Next quote
+        </button>
     </>
   )
 }
