195db283b2378f87fc6c749c120f82fb2a2fbcbe
Rafael <f.rafael.temich24@gmail.com>
2022-10-11 12:03:34 -0500

    Done

---
+++ src/07-tarea-memo/Padre.jsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useMemo } from 'react'
+import { Hijo } from './Hijo'
+import { useState } from 'react';
+
+export const Padre = () => {
+
+    const numeros = [2,4,6,8,10];
+    const [valor, setValor] = useState(0);
+
+    const incrementar = useCallback( (num) => {
+        // if (num === undefined) return;
+        setValor( (value) => value + num );
+    }, [])
+
+    // const incrementar = ( num ) => {
+    //     setValor( valor + num )
+    // }
+
+
+    return (
+        <div>
+            <h1>Padre</h1>
+            <p> Total: { valor } </p>
+
+            <hr />
+
+            {
+                numeros.map( n => (
+                    <Hijo 
+                        key={ n }
+                        numero={ n }
+                        incrementar={ incrementar }
+                    />
+                ))
+            }
+            {/* <Hijo /> */}
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,10 @@ import { Layout } from './05-useLayoutEffect/Layout'
 import { Memorize } from './06-memos/Memorize'
 import { MemoHook } from './06-memos/MemoHook'
 import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   // </React.StrictMode>
 )
